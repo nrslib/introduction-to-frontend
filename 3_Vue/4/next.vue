22b9d495aa287e309572e8144e5462286d0f7a55
nrslib <masanobu.naruse@gmail.com>
2022-05-19 10:06:09 +0900

    upgrade cdn

--- 3_Vue/4/next.vue
+++ 3_Vue/4/next.vue
@@ -3,32 +3,8 @@
     <h1>Hello, world!</h1>
     <section>
       <h2>Next</h2>
+      <p>next page.</p>
+      <router-link to="/">Go back</router-link>
     </section>
   </div>
 </template>
-
-<script>
-module.exports = {
-  data: function() {
-    return {
-      data: '',
-      message: ''
-    }
-  },
-  methods: {
-    async executeButtonClick() {
-      this.message = 'execute button clicked';
-      const self = this;
-      await axios.get('http://118.27.26.228/my-data.php')
-        .then(function(response) {
-          self.data = response.data['date'];
-          self.message = 'data received';
-        });
-    },
-    deleteButtonClick() {
-      this.message = 'delete button clicked';
-      this.data = '';
-    }
-  }
-}
-</script>
